6c936aad9c589f6ae75a1591608b90ccc3a421be
kevin-krause <contatokevinkrause@gmail.com>
2023-08-16 13:29:13 -0300

    primeiro commit home e header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import './Header.css'
+import Login from './Login/Login'
+
+const Header = () => {
+    return (
+        <div className="container">
+            <div className="mainHeader">
+                <div>Logo</div>
+                <div className="searchbar">
+                    <img src="#" alt="logo" />
+                    <input type="text"></input>
+                </div>
+                <button className="button">Anuncie seus itens</button>
+                <Login />
+            </div>
+
+            <div className="cabecalho">
+                <a href="/">Home</a>
+                <a href="/">Booking and Checkout</a>
+                <a href="/">Listing Page</a>
+                <a href="/">Wishlist</a>
+            </div>
+        </div>
+    )
+}
+
+export default Header
---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,20 @@
+import Header from '../Header/Header'
+import './Home.css'
+
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div>
+            <Header />
+            <div className="container">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Architecto corporis voluptas quo optio amet non consequatur
+                saepe ratione deleniti deserunt animi dolor aspernatur esse
+                quisquam, cupiditate minus dolores quasi suscipit!
+            </div>
+        </div>
+    )
+}
+
+export default Home
